8aa8946bbbc9f2549e431803d540f1200c1876cb
minerva421 <minerva421.ai@gmail.com>
2021-10-15 23:58:40 +0900

    ch7_2 내용 추가

--- ch7_유용한_문법/Generic/Program.cs
+++ ch7_유용한_문법/Generic/Program.cs
@@ -27,6 +27,8 @@ namespace ch7_1_Generic
     // T는 Type, Template의 약자
     class MyList<T>
     {
+        // obejct 타입으로 선언할 수 있지만 박싱, 언박싱을 거쳐야 하기 떄문에 성능상 좋지 않음
+        // object[] arr = new object[10];
         T[] arr = new T[10];
 
         public T GetItem(int i)
@@ -42,9 +44,9 @@ namespace ch7_1_Generic
 
     // Generic Type에 조건을 추가할 수 있다
     class MyListStruct<T> where T : struct   // 여기서 T는 값 형식이어야 함.
-                                             // class는 참조 형식,
-                                             // new()는 어떠한 인자도 받지 않는 기본 생성자가 있어야 함.
-                                             // Monster는 Monster나 Monster를 상속받은 클래스가 들어가야 함
+                                             // : class는 참조 형식,
+                                             // : new()는 어떠한 인자도 받지 않는 기본 생성자가 있어야 함.
+                                             // : Monster는 Monster나 Monster를 상속받은 클래스가 들어가야 함
     {
         T[] arr = new T[10];
 
@@ -64,7 +66,8 @@ namespace ch7_1_Generic
 
         static void Main(string[] args)
         {
-            // object를 사용하는 방법
+            // object를 사용하는 방법.
+            // 어떤 타입이든 다 소화가 가능함
             object obj = 3;
             object obj2 = "Hello";
 
@@ -75,7 +78,7 @@ namespace ch7_1_Generic
             // object는 int, string의 부모 클래스임. object를 상속 받아서 구현
             // int는 스택에 들어가는 정보. 복사 타입의 변수
             // object는 참조 타입. 힙에 메모리를 할당 해서 거기 숫자를 저장한 다음 꺼내올 때는 힙에서 숫자를 꺼내서 스택에 저장 (박싱, 언박싱)
-            // 따라서 전부 object 타입으로 선언하는 것은 무리가 있디
+            // 따라서 전부 object 타입으로 선언하는 것은 성능상에 무리가 있디
 
             MyList<int> myIntList = new MyList<int>();
             MyList<float> myFloatList = new MyList<float>();
---
+++ ch7_유용한_문법/ch7_2_Interface/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ch7_2_Interface
+{
+    class Monster
+    {
+        public virtual void Shout() { } // 몬스터는 무조건 shout 기능이 있어야함. 가상함수로 shout 정의. 
+    }
+
+    class Orc : Monster
+    {
+        public override void Shout()
+        {
+            Console.WriteLine("Orc Shout!");
+        }
+    }
+
+    class Skeleton : Monster
+    {
+        public override void Shout()  // Shout를 만들지 않아도 문법적으로 오류는 없음. 자식 클래스에서 Shout의 구현은 선택이다.
+        {
+            Console.WriteLine("Skeleton Shout!");
+        }
+    }
+
+    // Shout의 구현을 강제하고 싶을때는 abstract 사용
+
+    abstract class Monster2    // abstract를 붙이면 추상클래스 
+    {
+        public abstract void Shout();  // Shout(){}; 본문이 존재하면 안됨. 본문이 존재하지 않기 떄문에 반드시 파생 클래스에서 본문을 정의해야함.
+    }
+
+    class Orc2 : Monster2
+    {
+        public override void Shout()
+        {
+            Console.WriteLine("Orc Shout!");
+        }
+    }
+
+    class Skeleton2 : Monster2
+    {
+        public override void Shout()  // Shout를 만들지 않아도 문법적으로 오류는 없음. 자식 클래스에서 Shout의 구현은 선택이다.
+        {
+            Console.WriteLine("Skeleton Shout!");
+        }
+    }
+
+    // 공중에 날아가는 유닛을 만들기 위해 Flyable class 정의. Fly() 구현을 강제한다고 가정. 
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Monster m = new Monster();
+        }
+    }
+}
