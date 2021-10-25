4526d3c3a15b96615eb61aaf320a56b4996fac09
minerva421 <minerva421.ai@gmail.com>
2021-10-25 18:00:53 +0900

    Process사용하기 - CMD 명령어사용

---
+++ MultiThreading/ThreadClass/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadClass
+{
+    class Helper
+    {
+        // 다른 클레스의 메서드를 쓰레드에 호출하기 위해서는
+        // 해당 클래스의 객체를 생성한 후(혹은 외부로부터 전달받은 후)
+        // 그 객체의 메서드를 델리게이트로 Thread에 전달하면 된다
+        public void Run()
+        {
+            Console.WriteLine("Helper.Run");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().DoTest();
+        }
+
+        void DoTest()
+        {
+            // 새로운 쓰레드에서 run() 실행
+            // Run 메서드를 입력받아 ThreadStart 델리게이트 타입 객체를 생성한 후 Thread  클래스 생성자에 전달
+            Thread t1 = new Thread(new ThreadStart(Run));
+            t1.Start();
+
+            // 메인 쓰레드에서 Run() 실행
+            Run();
+
+            // 컴파일러가 Run() 메서드의 함수 프로토타입으로부터
+            // ThreadStart 델리게이트 객체를 추론하여 생성함
+            Thread t2 = new Thread(Run);
+            t2.Start();
+
+            // 익명메서드를 사용하여 쓰레드 생성
+            Thread t3 = new Thread(delegate()
+            {
+                Run();
+            });
+            t3.Start();
+
+            // 람다식을 사용하여 쓰레드 생성
+            Thread t4 = new Thread(() => Run());
+            t4.Start();
+
+            // 간략한 표현
+            new Thread(() => Run()).Start();
+
+            // Helper 클래스의 Run메서드 호출
+            Helper helper = new Helper();
+            Thread t5 = new Thread(helper.Run);
+            t5.Start();
+        }
+
+        void Run()
+        {
+            Console.WriteLine($"Thread#{Thread.CurrentThread.ManagedThreadId} : Begin");
+            Thread.Sleep(1000);
+            Console.WriteLine($"Thread#{Thread.CurrentThread.ManagedThreadId} : End");
+        }
+    }
+}
---
+++ Process/UseCmd/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+
+namespace UseCmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //[DllImport("kernel32.dll", SetLastError = true)]
+            //[return: MarshalAs(UnmanagedType.Bool)]
+            //static extern bool AllocConsole();
+
+            try
+            {
+                //AllocConsole();
+                ProcessStartInfo proInfo = new ProcessStartInfo();
+                Process pro = new Process();
+
+                // 실행할 파일명 입력 
+                //proInfo.FileName = "cmd.exe";
+                proInfo.FileName = @"cmd";
+                // cmd창 보이기(false). 보이지않기(true)
+                proInfo.CreateNoWindow = true;
+                // ??
+                proInfo.UseShellExecute = false;
+                // cmd 데이터 받기
+                proInfo.RedirectStandardOutput = true;
+                // cmd 데이터 보내기
+                proInfo.RedirectStandardInput = true;
+                // cmd 오류내용 받기
+                proInfo.RedirectStandardError = true;
+
+                // Process 실행 정보 추가
+                pro.StartInfo = proInfo;
+                // Process 시작
+                pro.Start();
+
+                // cmd에 보낼 명령어를 입력
+                pro.StandardInput.WriteLine(@"dir/w");
+                pro.StandardInput.Close();
+
+                // 결과값을 리턴
+                string resultValue = pro.StandardOutput.ReadToEnd();
+                pro.WaitForExit();
+                pro.Close();
+
+
+                // 결과값 확인
+                Console.WriteLine(resultValue);
+
+                Console.ReadLine();
+            }
+            catch (Win32Exception w)
+            {
+                Console.WriteLine(w.Message);
+                Console.WriteLine(w.ErrorCode.ToString());
+                Console.WriteLine(w.NativeErrorCode.ToString());
+                Console.WriteLine(w.StackTrace);
+                Console.WriteLine(w.Source);
+                Exception e = w.GetBaseException();
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
