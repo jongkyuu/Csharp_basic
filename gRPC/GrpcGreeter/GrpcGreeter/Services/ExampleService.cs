ac78808e8a38cd9abfc39780ad66586015007087
minerva421 <minerva421.ai@gmail.com>
2022-04-18 18:13:51 +0900

    ASP.net Core 실습 - 컨트롤러 추가

---
+++ gRPC/GrpcGreeter/GrpcGreeter/Services/ExampleService.cs
@@ -0,0 +1,70 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcGreeter
+{
+    public class ExampleService : Example.ExampleBase
+    {
+        private readonly ILogger<ExampleService> _logger;
+
+        public ExampleService(ILogger<ExampleService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override async Task<ExampleResponse> UnaryCall(ExampleRequest request, ServerCallContext context)
+        {
+            var response = new ExampleResponse();
+            return await Task.FromResult(response);
+        }
+
+        public override async Task StreamingFromServer(ExampleRequest request, IServerStreamWriter<ExampleResponse> responseStream, ServerCallContext context)
+        {
+            while(!context.CancellationToken.IsCancellationRequested)
+            {
+                await responseStream.WriteAsync(new ExampleResponse { Message = "StreamingFromServer" });
+                await Task.Delay(TimeSpan.FromSeconds(1000), context.CancellationToken);
+            }
+        }
+
+        public override async Task<ExampleResponse> StreamingFromClient(IAsyncStreamReader<ExampleRequest> requestStream, ServerCallContext context)
+        {
+            //while (await requestStream.MoveNext())
+            //{
+            //    var message = requestStream.Current;
+            //}
+
+            await foreach(var message in requestStream.ReadAllAsync())
+            {
+                //...
+            }
+
+            return new ExampleResponse();
+        }
+
+        public override async Task StreamingBothWays(IAsyncStreamReader<ExampleRequest> requestStream,
+            IServerStreamWriter<ExampleResponse> responseStream, ServerCallContext context)
+        {
+            //await foreach (var message in requestStream.ReadAllAsync())
+            //{
+            //    await responseStream.WriteAsync(new ExampleResponse());
+            //}
+
+            var readTask = Task.Run(async () =>
+            {
+                await foreach (var message in requestStream.ReadAllAsync())
+                {
+                    // Process request
+                }
+            });
+
+            while (!readTask.IsCompleted)
+            {
+                await responseStream.WriteAsync(new ExampleResponse()); 
+                await Task.Delay(TimeSpan.FromSeconds(1000), context.CancellationToken);
+            }
+        }
+    }
+}
---
+++ gRPC/MvcMovie/MvcMovie/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie
+{
+    public class Program
+    {
+
+        // ¸µÅ© : https://docs.microsoft.com/ko-kr/aspnet/core/tutorials/first-mvc-app/start-mvc?view=aspnetcore-6.0&tabs=visual-studio
+
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
