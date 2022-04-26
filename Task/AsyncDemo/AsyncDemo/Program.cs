99eed98feb84a2066ebe38983351ae2c3634c5f9
minerva421 <minerva421.ai@gmail.com>
2022-04-26 18:12:33 +0900

    MS Doc - AsyncDemo1

---
+++ Task/AsyncDemo/AsyncDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 호출자 스레드(메인 스레드)를 Block하지 않는다
+            //TaskTest();
+            TaskTest2();
+            Console.WriteLine("End Main");
+            Console.Read();
+
+        }
+
+        static void TaskTest()
+        {
+            Task.Delay(5000);  // 현재 스레드 중지시키지 않음 
+            //Thread.Sleep(5000);  // 현재 스레드 중지시킴
+            Console.WriteLine("End AsyncFunc");
+        }
+
+        static async void TaskTest2()
+        {
+            await Task.Delay(5000);
+            Console.WriteLine("End AsyncFunc");
+
+        }
+    }
+}
---
+++ Task/AsyncDemo/AsyncDemo2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncDemo2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //Task t = TaskTest();
+            Task<int> t2 = TaskTestDB();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Do Something Before TaskTest");
+            }
+
+            //await t;
+            int UID = await t2;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Do Something After TaskTest");
+            }
+
+            Console.WriteLine($"UID : {UID}");
+            Console.ReadLine();
+        }
+
+        static async Task TaskTest()
+        {
+            await Task.Delay(5000);
+            Console.WriteLine("TaskTest Done");
+        }
+
+        static async Task<int> TaskTestDB()
+        {
+            //await Task.Delay(5000);
+            //Console.WriteLine("TaskTestDB Done");
+            //int UID = 100;
+
+            int UID = await GetUID();
+            Console.WriteLine("TaskTestDB Done");
+
+            return UID;
+        }
+
+        static async Task<int> TaskTestDB2()
+        {
+            int UID = await Task.Run(() => GetUID2());
+            Console.WriteLine("TaskTestDB Done");
+
+            return UID;
+        }
+
+        static async Task<int> GetUID()
+        {
+            await Task.Delay(5000);
+            // DB에서 UID 가져오는 작업
+            int UID = 100;
+            return UID;
+        }
+
+        static int GetUID2()
+        {
+            Thread.Sleep(5000);
+            // DB에서 UID 가져오는 작업
+            int UID = 100;
+            return UID;
+        }
+    }
+}
