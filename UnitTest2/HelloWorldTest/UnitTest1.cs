f879888d4d0b2b780193b475667482a2eb9fda36
minerva421 <minerva421.ai@gmail.com>
2022-06-13 17:33:59 +0900

    UnitTest 추가

---
+++ UnitTest2/HelloWorldTest/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace HelloWorldTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            HelloWorld.Program
+        }
+    }
+}
